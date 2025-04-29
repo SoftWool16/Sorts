808ae4160ad07daf32cf37e50e609e28f504852c
andre1315 <korabelskijandrej2010@gmail.com>
2025-04-29 22:07:33 +0300

    сортАндр

--- PY_Sorts/sortAndr_1.py
+++ PY_Sorts/sortAndr_1.py
@@ -1,38 +1,16 @@
 num = int(input())
 import random
-string = ""      #создание пустого списка
 a = list(range(num)) #ввод размера массива
 random.shuffle(a)   #перемешивание массива
-for b in a:
-    string += (" | " + str(b))
-print(string)
-
-
-def ids(n1, n2):
-    if n1 > n2:
-        s = a.pop(n1)
-        a.insert(n2, s)
-        print(a)
-        string2 = ""
-        s = a.pop(n1)
-        a.insert(n2, s)
-        for b in a:
-            string2 += (" | " + str(b))
-        print(string2)
-    else:
-        print("--------код хуйня--------")
-
-
-def lst(num):
-    for n1 in range(num):
-        for n2 in range(num):
-            n2 += 1
-            if n1 > n2:
-                ids(n1, n2)
-
-
-for s in range(num):
-    if s <= num:
-        lst(num)
-    else:
-        print("--------конец--------")
+print(a)
+m = 0
+while m<(num-1):
+    n = m
+    while n<num:
+        if a[n] < a[m]:
+            a[n],a[m] = a[m],a[n]
+            n += 1
+        else:
+            n += 1
+    m += 1
+print(a)
